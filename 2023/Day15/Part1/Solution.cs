c5f7cae00ceab7b8758d935b1b73153b96375f9b
Dustin Joosen <dustinjoosen2003@gmail.com>
2023-12-16 13:01:08 +0100

    Day 15

---
+++ 2023/Day15/Part1/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2023.Day15.Part1
+{
+    [Display(Name = "Day 15 A")]
+    public class Solution : BaseSolution
+    {
+        public override void Run()
+        {
+            int total = 0;
+            foreach (string sequence in Lines.First().Split(","))
+            {
+                int value = GetHashCode(sequence);
+                Console.WriteLine($"{sequence} - {value}");
+                total += value;
+            }
+            Console.WriteLine(total);
+        }
+
+        private int GetHashCode(string text)
+        {
+            int code = 0;
+            foreach (char c in text)
+            {
+                code += (int)c;
+                code *= 17;
+                code = code % 256;
+            }
+
+            return code;
+        }
+    }
+}
